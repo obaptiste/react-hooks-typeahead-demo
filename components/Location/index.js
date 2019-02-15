fe0812608b9c898ceaa1d59846e475b9eafee729
anmolmahatpurkar <anmolmahatpurkar@gmail.com>
2019-02-15 15:02:47 +0530

    Clearing Selected Value

--- components/Location/index.js
+++ components/Location/index.js
@@ -1,26 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { useLocations } from '../../hooks/locations';
-import { Container, Input, Options, Option } from './styles';
+import { useLocations } from "../../hooks/locations";
+import { Container, Input, Options, Option } from "./styles";
 
 const Location = ({ value, onChange, placeholder }) => {
-	const [searchString, setSearchString] = useState('');
+	const [searchString, setSearchString] = useState("");
 	const [showOptions, setShowOptions] = useState(false);
 	const { locations, isLoading } = useLocations(searchString);
 
+	const handleInputChange = e => {
+		if(!(value.name && (e.target.value.length < value.name.length))){ // For Clearing Selected Input
+			setSearchString(e.target.value);
+		}
+		onChange({});
+	}
+	
 	return <Container>
 		<Input
 			value={(value || {}).name || searchString}
-			onChange={(e) => setSearchString(e.target.value)}
+			onChange={handleInputChange}
 			onFocus={() => setShowOptions(true)}
 			onBlur={() => setTimeout(() => setShowOptions(false), 200)}
 			placeholder={placeholder}
 		/>
 		{isLoading && <span>Loading...</span>}
-		{showOptions && <Options>{locations.map((item, id) => (
+		{showOptions && (<Options>{locations.map((item, id) => (
 			<Option key={id} onClick={() => onChange(item)}>{item.name}</Option>
-		))}</Options>}
+		))}</Options>)}
 	</Container>
-}
+};
 
 export default Location;
